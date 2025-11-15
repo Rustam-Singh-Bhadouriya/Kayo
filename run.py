0485d1bbddc1c1be94eb5dfdb469cdb80c7db4a2
Rustam-Singh-Bhadouriya <rustambhadouriya7@gmail.com>
2025-11-14 23:27:04 -0800

    Update run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+# Beta Only
+
+
+# Copy Right Comment C.
+"""
+THIS WILL READY WHEN YOU GET FRONTEND OF THIS PART
+
+WHAT THIS FILE DO?
+this file is only for merging your backend and frontend!
+
+backend stable had released btw
+but Frontend need some time!
+
+
+NOTE: You are Free To Build Frontend and Backend or Edit this file left this comment
+AND create pull requests and build and learn, put it on your Resume!
+
+"""
